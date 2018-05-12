7a728b2de51ca03d9e21530df4adf623777e85aa
Vitor Hugo Lollato <vitor.lollato@gmail.com>
2018-05-12 15:25:51 -0300

    Identation

--- Ex16/main.c
+++ Ex16/main.c
@@ -14,25 +14,25 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 void inverterString(char *string){
-char *normal=string;//guardando referencia para string
-char *inicio; //referencia inicio de cada palavra
-char *fim;//referencia fim de cada palavra
-char *inv=(char*)malloc(sizeof(strlen(string)+1));//allocando espaco para string invertida
-char *invAux=inv;//referencia posicoes da nova string
+	char *normal=string;//guardando referencia para string
+	char *inicio; //referencia inicio de cada palavra
+	char *fim;//referencia fim de cada palavra
+	char *inv=(char*)malloc(sizeof(strlen(string)+1));//allocando espaco para string invertida
+	char *invAux=inv;//referencia posicoes da nova string
 
-while(*string != '.'){
-        inicio=string;
+	while(*string != '.'){
+		inicio=string;
 		do{
-            string++;
-            fim=string;
-		}while(*string != ' ' && *string!='.');
-        if(*string == '.'){
-            fim--;
-        }
+			string++;
+			fim=string;
+		}while(*string != ' ' && *string != '.');
+		if(*string == '.'){
+			fim--;
+		}
 		while(fim!=inicio-1){
-            *invAux=*fim;
-            fim--;
-            invAux++;
+			*invAux=*fim;
+			fim--;
+			invAux++;
 		}
 	}
 	invAux--;
